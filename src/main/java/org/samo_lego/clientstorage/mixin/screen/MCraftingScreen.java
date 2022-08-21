2b83f6920bedee55f5c2ff5f6afd72f7e2c18107
samolego <34912839+samolego@users.noreply.github.com>
2022-08-21 09:32:23 +0200

    Fix bg in small screen

--- src/main/java/org/samo_lego/clientstorage/mixin/screen/MCraftingScreen.java
+++ src/main/java/org/samo_lego/clientstorage/mixin/screen/MCraftingScreen.java
@@ -63,17 +63,26 @@ public abstract class MCraftingScreen extends AbstractContainerScreen<CraftingMe
 
     @Inject(method = "renderBg(Lcom/mojang/blaze3d/vertex/PoseStack;FII)V", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void addBackground(PoseStack matrices, float delta, int mouseX, int mouseY, CallbackInfo ci, int startX, int y) {
-        //RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        //Minecraft.getInstance().getTextureManager().bindForSetup(TEXTURE_SEARCH);
         RenderSystem.setShaderTexture(0, TEXTURE_SEARCH);
         final int SEARCHBAR_HEIGHT = 71;
         final int SEARCHBAR_BOTTOM_HEIGHT = 24;
         final int SEARCHBAR_BOTTOM_START = 111;
         final int SEARCHBAR_WIDTH = 195;
-        //((HandledScreenAccessor) craftingScreen).setBackgroundHeight(SEARCHBAR_HEIGHT);
 
-        self.blit(matrices, (self.width - SEARCHBAR_WIDTH) / 2, y - SEARCHBAR_HEIGHT - 6, 0, 0, SEARCHBAR_WIDTH, SEARCHBAR_HEIGHT);
-        self.blit(matrices, (self.width - SEARCHBAR_WIDTH) / 2, y - SEARCHBAR_BOTTOM_HEIGHT, 0, SEARCHBAR_BOTTOM_START, SEARCHBAR_WIDTH, SEARCHBAR_BOTTOM_HEIGHT);
+        final int x = startX - 10;
+        // Added inventory
+        self.blit(matrices, x, y - SEARCHBAR_HEIGHT - 6, 0, 0, SEARCHBAR_WIDTH, SEARCHBAR_HEIGHT);
+        self.blit(matrices, x, y - SEARCHBAR_BOTTOM_HEIGHT, 0, SEARCHBAR_BOTTOM_START, SEARCHBAR_WIDTH, SEARCHBAR_BOTTOM_HEIGHT);
+
+
+        this.searchBox.render(matrices, mouseX, mouseY, delta);
+        int topX = this.leftPos + 165;
+        int topY = this.topPos - 23;
+        int k = topY + 54;
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, CREATIVE_TABS_LOCATION());
+        this.blit(matrices, topX, topY + (int) ((float) (k - topY - 17) * this.scrollOffs), 232, 0, 12, 15);
     }
 
     @Inject(method = "<init>", at = @At("TAIL"))
@@ -188,18 +197,4 @@ public abstract class MCraftingScreen extends AbstractContainerScreen<CraftingMe
             ci.cancel();
         }
     }
-
-    @Inject(method = "renderBg", at = @At("TAIL"))
-    private void renderBg(PoseStack matrices, float delta, int mouseX, int mouseY, CallbackInfo ci) {
-        this.searchBox.render(matrices, mouseX, mouseY, delta);
-        int x = this.leftPos + 165;
-        int y = this.topPos - 23;
-        int k = y + 54;
-
-
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, CREATIVE_TABS_LOCATION());
-        this.blit(matrices, x, y + (int) ((float) (k - y - 17) * this.scrollOffs), 232, 0, 12, 15);
-    }
-
 }
