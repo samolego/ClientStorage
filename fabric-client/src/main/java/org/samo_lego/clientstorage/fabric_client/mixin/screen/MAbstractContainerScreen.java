b1c8d12a05b7ff869fc2a0337b2879b566fafb2a
EnderLord0042 <55165281+EnderLord0042@users.noreply.github.com>
2022-10-22 23:58:58 -0700

    Add small text rendering for overstacked items (#9)
    * Add small text rendering for overstacked items
    
    * Apply small text rendering to all remote slots
    
    * Only Render amount text if stack has more than 1 item
    
    Co-authored-by: EnderLord0042 <EnderLord0042@users.noreply.github.com>

--- fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/mixin/screen/MAbstractContainerScreen.java
+++ fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/mixin/screen/MAbstractContainerScreen.java
@@ -1,16 +1,26 @@
 package org.samo_lego.clientstorage.fabric_client.mixin.screen;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.math.Matrix4f;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 import org.samo_lego.clientstorage.fabric_client.inventory.RemoteSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerScreen.class)
@@ -21,6 +31,9 @@ public class MAbstractContainerScreen extends Screen {
 
     @Unique
     private Slot slot;
+
+    @Unique
+    boolean renderWithSmallText;
 
     protected MAbstractContainerScreen(Component component) {
         super(component);
@@ -33,7 +46,10 @@ public class MAbstractContainerScreen extends Screen {
 
     @ModifyVariable(method = "renderSlot", at = @At(value = "STORE"))
     private String changeItemCountLabel(String label) {
+        this.renderWithSmallText = false; // Use normal-sized text for normal slots
         if (this.slot instanceof RemoteSlot) {
+            this.renderWithSmallText = true; // Use small text for remote slots
+
             // Custom count label
             int count = this.slot.getItem().getCount();
 
@@ -44,5 +60,23 @@ public class MAbstractContainerScreen extends Screen {
             }
         }
         return label;
+    }
+
+    @Redirect(method = "renderSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderGuiItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V"))
+    private void renderGuiItemDecorationsWithDifferentTextSize(ItemRenderer itemRenderer, Font fontRenderer, ItemStack stack, int x, int y, @Nullable String countLabel) {
+        if (!stack.isEmpty()) { // Ensure ItemStack isn't empty before decorating
+            if (renderWithSmallText) {
+                itemRenderer.renderGuiItemDecorations(fontRenderer, stack, x, y, ""); // Render other Decorations
+                if (stack.getCount() > 1) { // Only render amount text if stack has more than 1 item
+                    countLabel = countLabel == null ? String.valueOf(stack.getCount()) : countLabel; // Get count string if countLabel is null
+                    PoseStack textMatrixStack = new PoseStack(); // Create new matrix stack for transforming text size
+                    textMatrixStack.scale(0.5F, 0.5F, 1); // Scale matrix stack to make text smaller
+                    textMatrixStack.translate(0, 0, itemRenderer.blitOffset + itemRenderer.ITEM_COUNT_BLIT_OFFSET); // Offset text z position so that it is in front of item
+                    fontRenderer.drawShadow(textMatrixStack, countLabel, x * 2 + 31 - fontRenderer.width(countLabel), y * 2 + 23, ChatFormatting.WHITE.getColor()); // Render count label
+                }
+            } else {
+                itemRenderer.renderGuiItemDecorations(fontRenderer, stack, x, y, countLabel); // Render Decorations Normally
+            }
+        }
     }
 }
