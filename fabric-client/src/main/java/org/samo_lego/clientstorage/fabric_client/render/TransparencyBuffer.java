3682c4a7fef27c1aa20c836dec9eec76f8ecfd27
samolego <34912839+samolego@users.noreply.github.com>
2023-10-08 12:28:18 +0200

    Fix storage presets rendering

--- fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/render/TransparencyBuffer.java
+++ fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/render/TransparencyBuffer.java
@@ -5,8 +5,10 @@ import com.mojang.blaze3d.pipeline.TextureTarget;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.GameRenderer;
 import org.lwjgl.opengl.GL30;
 
 /**
@@ -44,18 +46,19 @@ public class TransparencyBuffer {
 
         // Render the custom framebuffer's contents with transparency into the main buffer
         RenderSystem.setShaderTexture(0, framebuffer.getColorTextureId());
-        /*Window window = Minecraft.getInstance().getWindow();
-        graphics.blit(
-                framebuffer.getColorTextureId(),
-                0,                            // x
-                0,                            // y
-                window.getGuiScaledWidth(),   // width
-                window.getGuiScaledHeight(),  // height
-                0,                            // left-most coordinate of the texture region
-                framebuffer.height,           // top-most coordinate of the texture region
-                framebuffer.width,            // width of the texture region
-                -framebuffer.height          // height of the texture region
-        );*/
+        Window window = Minecraft.getInstance().getWindow();
+        int width = window.getGuiScaledWidth();
+        int height = window.getGuiScaledHeight();
+        RenderSystem.setShaderTexture(0, framebuffer.getColorTextureId());
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        final var matrix4f = graphics.pose().last().pose();
+        BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        bufferBuilder.vertex(matrix4f, 0, 0, 0).uv(0, 1).endVertex();
+        bufferBuilder.vertex(matrix4f, 0, height, 0).uv(0, 0).endVertex();
+        bufferBuilder.vertex(matrix4f, width, height, 0).uv(1, 0).endVertex();
+        bufferBuilder.vertex(matrix4f, width, 0, 0).uv(1, 1).endVertex();
+        BufferUploader.drawWithShader(bufferBuilder.end());
     }
 
     public static void postInject() {
